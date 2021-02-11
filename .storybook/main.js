56deaf09cb08082e63d5e3ea94f663f9e4d4e40c
Roman Kr <roman7@gmail.com>
2021-02-11 23:21:54 +0200

    next.js with tailwind css and storybook

---
+++ .storybook/main.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  "webpackFinal": async (config) => {
+    config.module.rules.push({
+      test: /\.s?css$/,
+      use: [
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              ident: 'postcss',
+              plugins: [
+                require('tailwindcss'),
+                require('autoprefixer'),
+              ],
+            }
+          },
+        },
+      ],
+      include: path.resolve(__dirname, '../'),
+    })
+    return config
+  }
+}
